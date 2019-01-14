5dedce89fcb67f1b34222e3330cba51fba000ffd
shuyuhan0303 <m15717828167@163.com>
2019-01-14 15:31:21 +0800

    bottom

--- src/components/common/components/Bottom.vue
+++ src/components/common/components/Bottom.vue
@@ -16,7 +16,7 @@ import { oppid } from "@/components/axios/api";
 import axios from "axios";
 import secret from "@/utils/utils";
 var ajax = new XMLHttpRequest();
-ajax.open("get", "http://csapi.nguiba.com/api/makeCode");
+ajax.open("get", "http://www.nguiba.com/api/makeCode");
 ajax.send();
 ajax.onreadystatechange = function() {
   if (ajax.readyState == 4 && ajax.status == 200) {
