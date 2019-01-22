62008b658031b4b9d8eaa584c17294ac4fae0673
shuyuhan0303 <m15717828167@163.com>
2019-01-22 14:09:11 +0800

    111

--- src/components/axios/url.js
+++ src/components/axios/url.js
@@ -1,6 +1,6 @@
 
-const HOST = process.env.NODE_ENV  !== 'production' ? '/api' : 'http://csapi.nguiba.com/api'
-export default {
+const HOST = process.env.NODE_ENV  !== 'production' ? '/api' : 'http://api.ngba.cn/api'                                                      
+export default { 
     Hallowmas:HOST,
     //接口代理配置
   }
