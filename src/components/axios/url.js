03013621b66c56a0de74de38190f7b728619d4fb
william817 <934587766@qq.com>
2019-01-22 17:37:41 +0800

    test

--- src/components/axios/url.js
+++ src/components/axios/url.js
@@ -1,5 +1,5 @@
 
-const HOST = process.env.NODE_ENV  !== 'production' ? '/api' : 'http://api.ngba.cn/api'                                                      
+const HOST = process.env.NODE_ENV  !== 'production' ? '/api' : 'http://wechat.ngba.cn/api'                                                      
 export default { 
     Hallowmas:HOST,
     //接口代理配置
