27459e785f74cf24424c6e2f837a56db80074a71
这里换上你的用户名 <m15717828167@163.com>
2019-03-18 14:04:27 +0800

    分享isok

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -34,10 +34,10 @@ export default {
         // }
         // location.href=URL;
         if (iswx) {
-          const code = localStorage.getItem("houjiwxa");
+          const code = localStorage.getItem("hou");
           if (code == null || code == undefined) {
             location.href = URL;
-            localStorage.setItem("houjiwxa", "2");
+            localStorage.setItem("hou", "81");
           }
         }
       })
@@ -61,7 +61,7 @@ export default {
       // console.log(store.state.accessToken);
       // console.log(store.state.openid);
       // console.log(store.state.token);
-      if (localStorage.getItem("houjiwxa") == 2) {
+      if (localStorage.getItem("hou") == 81) {
         const code = url.split("code=")[1].split("&")[0];
         console.log(code);
         // const newurl = url.split("code=")[0];
