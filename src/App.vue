70bc354e1aa64a156687bf5c613df2ed45c7b5af
这里换上你的用户名 <m15717828167@163.com>
2019-03-21 15:02:17 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 import Grant from "@/components/Authorization";
 import FadeAnimation from "@/components/common/Fade";
 import assign from "@/components/axios/assign.js";
+import { Code } from "@/components/axios/api";
 export default {
   name: "App",
   mixins: [assign],
@@ -21,12 +22,54 @@ export default {
     Grant,
     FadeAnimation
   },
+  created() {},
   mounted() {
     this.$toast({
       type: "loading",
       message: "加载中...",
       duration: "1200"
     });
+    var code = localStorage.getItem("code")
+    if(code == null) return
+    Code(code)
+      .then(res => {
+        console.log(res.data.data);
+        const data = res.data.data;
+
+        const accessTokens = data.access_token;
+        const openids = data.openid;
+        localStorage.setItem("accessTokens", accessTokens);
+        localStorage.setItem("openids", openids);
+        const tokens = data.token;
+        const unionid = data.unionid;
+        // const subscribe = data.subscribe;
+        // this.$store.commit({
+        //   type: "addIncrement",
+        //   accessTokens: accessTokens,
+        //   openids: openids,
+        //   tokens: tokens
+        //   // subscribe: subscribe
+        // });
+        // console.log(tokens)
+        // console.log(this.$store)
+        // this.$store.commit(types.LOGIN, tokens);
+        //如果没绑定手机号 跳到绑定页面
+        if (data.is_bind_mobile == 0) {
+          setTimeout(() => {
+            this.$router.push({
+              path: "/phone"
+              // query: {
+              //   dataObj: unionid
+              // }
+            });
+          }, 3000);
+        }
+        // window.location.href = newurl;
+        // this.reload();
+      })
+      .catch(err => {
+        console.log(err, "请求失败");
+      });
   }
   // methods: {
   //   reload() {
--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -2,7 +2,7 @@
 <script type="text/javascript">
 // import * as types from "@/components/vuex/types";
 import * as types from "@/components/vuex/types";
-import { Code } from "@/components/axios/api";
+
 import { huoqu } from "@/components/axios/api";
 // import { SignPackage } from "@/components/axios/api";
 let iswx =
@@ -55,56 +55,14 @@ export default {
     refrech() {
       // 拿到跳转后的链接
       const url = window.location.href;
-      console.log(url.split("?"));
       const code = url.split("code=")[1].split("&")[0];
       console.log(code);
-      const newurl = url.split("code=")[0];
-      const uilLength = url.split("?");
-
-      Code(code)
-        .then(res => {
-          console.log(res.data.data);
-          const data = res.data.data;
-
-          const accessTokens = data.access_token;
-          const openids = data.openid;
-          localStorage.setItem("accessTokens", accessTokens);
-          localStorage.setItem("openids", openids);
-          const tokens = data.token;
-          const unionid = data.unionid;
-          // const subscribe = data.subscribe;
-          // this.$store.commit({
-          //   type: "addIncrement",
-          //   accessTokens: accessTokens,
-          //   openids: openids,
-          //   tokens: tokens
-          //   // subscribe: subscribe
-          // });
-          // console.log(tokens)
-          // console.log(this.$store)
-          // this.$store.commit(types.LOGIN, tokens);
-          //如果没绑定手机号 跳到绑定页面
-          if (data.is_bind_mobile == 0) {
-            setTimeout(() => {
-              this.$router.push({
-                path: "/phone"
-                // query: {
-                //   dataObj: unionid
-                // }
-              });
-            }, 3000);
-          }
-          // window.location.href = newurl;
-          // this.reload();
-        })
-        .catch(err => {
-          console.log(err, "请求失败");
-        });
-      //     localStorage.setItem("oances", "2");
+       localStorage.setItem("code", code);
+      // console.log(url.split("?"));
+      // const uilLength = url.split("?");
+      // if (uilLength.length == 1 || uilLength.length == 2) {
+      //   return false;
       // }
-      if (uilLength.length == 1 || uilLength.length == 2) {
-        return;
-      }
     }
   }
   //   destroyed() {
