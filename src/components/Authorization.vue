3fc1f902b599e8df5441e2dc140abb94b95e6cc8
这里换上你的用户名 <m15717828167@163.com>
2019-03-21 15:44:57 +0800

    style

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -55,50 +55,50 @@ export default {
     refrech() {
       // 拿到跳转后的链接
       const url = window.location.href;
-      console.log(url.split("code="));
-      const code = url.split("code=")[1].split("&")[0];
-      const newurl = url.split("code=")[0];
+      console.log(url);
+      // const code = url.split("code=")[1].split("&")[0];
+      // const newurl = url.split("code=")[0];
 
-      Code(code)
-        .then(res => {
-          console.log(res.data.data);
-          const data = res.data.data;
-          const accessTokens = data.access_token;
-          const openids = data.openid;
-          localStorage.setItem("accessTokens", accessTokens);
-          localStorage.setItem("openids", openids);
-          const tokens = data.token;
-          const unionid = data.unionid;
-          // const subscribe = data.subscribe;
-          // this.$store.commit({
-          //   type: "addIncrement",
-          //   accessTokens: accessTokens,
-          //   openids: openids,
-          //   tokens: tokens
-          //   // subscribe: subscribe
-          // });
-          // console.log(tokens)
-          // console.log(this.$store)
-          this.$store.commit(types.LOGIN, tokens);
-          //如果没绑定手机号 跳到绑定页面
-          if (data.is_bind_mobile == 0) {
-            setTimeout(() => {
-              this.$router.push({
-                path: "/phone"
-                // query: {
-                //   dataObj: unionid
-                // }
-              });
-            }, 3000);
-          }
-        })
-        .catch(err => {
-          console.log(err, "请求失败");
-        });
+      // Code(code)
+      //   .then(res => {
+      //     console.log(res.data.data);
+      //     const data = res.data.data;
+      //     const accessTokens = data.access_token;
+      //     const openids = data.openid;
+      //     localStorage.setItem("accessTokens", accessTokens);
+      //     localStorage.setItem("openids", openids);
+      //     const tokens = data.token;
+      //     const unionid = data.unionid;
+      //     // const subscribe = data.subscribe;
+      //     // this.$store.commit({
+      //     //   type: "addIncrement",
+      //     //   accessTokens: accessTokens,
+      //     //   openids: openids,
+      //     //   tokens: tokens
+      //     //   // subscribe: subscribe
+      //     // });
+      //     // console.log(tokens)
+      //     // console.log(this.$store)
+      //     this.$store.commit(types.LOGIN, tokens);
+      //     //如果没绑定手机号 跳到绑定页面
+      //     if (data.is_bind_mobile == 0) {
+      //       setTimeout(() => {
+      //         this.$router.push({
+      //           path: "/phone"
+      //           // query: {
+      //           //   dataObj: unionid
+      //           // }
+      //         });
+      //       }, 3000);
+      //     }
+      //   })
+      //   .catch(err => {
+      //     console.log(err, "请求失败");
+      //   });
 
-      setTimeout(() => {
-        window.location.href = newurl;
-      }, 1000);
+      // setTimeout(() => {
+      //   window.location.href = newurl;
+      // }, 1000);
     }
   }
   //   destroyed() {
